e2aebdbfcf22593fe7f84d8cc67fff5aac6340a1
Devon Sawatsky <novedevo@gmail.com>
2023-12-23 19:00:07 -0800

    add binary package with gifed dependency, write skeleton for dreamland builder interface

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io::Write;
+
+use fxyt::{render, ParseError};
+
+fn main() {
+    let Some(program) = std::env::args().nth(1) else {
+        eprintln!("Error: please pass the FXYT program as a command line argument.");
+        eprintln!(r#"For example: `fxyt "XY^"`."#);
+        eprintln!(r#"To run the empty program and produce a pure black image, run `fxyt ""`."#);
+        return;
+    };
+
+    let frames = match render(&program) {
+        Ok(frames) => frames,
+        Err(e) => {
+            eprintln!("Error: {e}");
+            return;
+        }
+    };
+
+    let mut gif = gifed::GifBuilder::default();
+    gif.set_resolution(256, 256);
+    //optional:
+    //gif.set_framerate(30);
+    //gif.set_palette(gifed::Palette::Simple);
+
+    for frame in &frames {
+        //option 1:
+        let gif_frame = GifFrameBuilder::from(frame.image);
+        gif_frame.set_interval(frame.interval); //if no interval is set on a frame, the gif builder will assign it one
+                                                //based on the framerate set above?
+        gif_frame.optimize_palette(); //or gif_frame.use_global_palette();
+        gif.add_frame(gif_frame.build()?);
+        //option 2:
+        gif.add_frame(frame.image, frame.interval).unwrap(); //could throw errors when given an image of the wrong resolution?
+    }
+
+    let gif = gif.build()?; //do a global palette calculation here if any frames don't have their own palettes?
+
+    let output_file = std::fs::File::create("output.gif")?;
+    output_file.write_all(gif.into_bytes())?
+}
